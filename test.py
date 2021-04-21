1b585cf63012335722b9ada478719b71232d187f
Denys Gergun <dgergun@rodanfields.com>
2021-04-21 18:03:42 +0300

    Adding test

---
+++ test.py
@@ -0,0 +1,2 @@
+import airflow_mailgun_email
+airflow_mailgun_email.email_mailgun.send_email_mailgun('gergund@gmail.com,dgergun@rodanandfields.com,ankarao123@gmail.com,aambepalli@rodanandfields.com','TEST','<b>Foo</b> bar',['/dev/null'], dryrun=True)
